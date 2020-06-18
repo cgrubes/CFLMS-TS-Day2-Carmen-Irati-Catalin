9741704dbdc3e0f21cfaf6557459bf78103cf61a
carmen <grubes@gmail.com>
2020-06-18 15:18:23 +0200

    added about page with bootstrap

---
+++ Carmen/vehicles.js
@@ -0,0 +1,11 @@
+var default_1 = /** @class */ (function () {
+    function default_1() {
+    }
+    return default_1;
+}());
+Vehicles;
+{
+    model = "";
+    fueltype = "";
+    seats = "";
+}
---
+++ Carmen/vehicles.ts
@@ -0,0 +1,5 @@
+class = Vehicles{
+    model= "";
+    fueltype = "";
+    seats = "";
+ }
