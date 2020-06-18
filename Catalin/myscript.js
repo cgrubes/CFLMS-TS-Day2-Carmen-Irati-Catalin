fa391f462e78ec34d3b17e98881fa4cdaa1d5bbf
Irati Larreina <irati_6@hotmail.com>
2020-06-18 13:50:50 +0200

    added some typescript

--- Catalin/myscript.js
+++ Catalin/myscript.js
@@ -16,19 +16,25 @@ var Person = /** @class */ (function () {
         this.img = img;
     }
     Person.prototype.myinfo = function () {
-        return [this.name, this.age, this.location, this.hobbies, this.music, this.img];
+        return [this.name, this.age, this.location, this.hobbies, this.music];
     };
     Person.prototype.myquote = function () {
-        return [this.qoute, this.img];
+        return this.qoute;
+    };
+    Person.prototype.myimage = function () {
+        return this.img;
     };
     return Person;
 }());
 var Profile = JSON.parse(json);
-//var person1 = new Person(Profile[0].name, Profile[0].age, Profile[0].location, Profile[0].hobbies, Profile[0].music, Profile[0].qoute, Profile[0].img);
+//var person1= new Person(Profile[0].name,Profile[0].age, Profile[0].location, Profile[0].hobbies, Profile[0].music, Profile[0].qoute, Profile[0].img);
 //console.log(person1.myinfo());
 //console.log(person1.myquote());
-
-
-for (let index in Profile){
-    document.getElementById
+//${person.myimage()}>
+for (var index in Profile) {
+    var person = new Person(Profile[index].name, Profile[index].age, Profile[index].location, Profile[index].hobbies, Profile[index].music, Profile[index].qoute, Profile[index].img);
+    document.getElementById("wrapper1").innerHTML +=
+        "<div id = \"card_" + index + "\" class = \"cardSmall\"> \n \t\t<div id = \"imageBox_" + index + "\" class = \"imageBoxBig\">\n \t\t\t<img id = \"image_" + index + "\" src = \"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500\">\n \t\t</div>\n \t\t<div id = \"quoteBox_" + index + "\">\n\t\t\t<h3> \n\t\t\t\t" + person.myquote() + "\n\t\t\t</h3>\n \t\t</div>\n \t</div>";
+    document.getElementById("wrapper2").innerHTML +=
+        "<div id = \"cardFav_" + index + " class = \"cardBig\"> \n \t\t<div id = \"imageBoxFav_" + index + "\" class = \"imageBoxBig\">\n \t\t\t<img id = \"imageFav_" + index + "\" src = \"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500\">\n \t\t</div>\n \t\t<div id = \"infoBox_" + index + "\">\n\t\t\t<h4> \n\t\t\t\tName: " + person.myinfo()[0] + "\n\t\t\t</h4>\n\t\t\t<p>\n\t\t\t\tAge: " + person.myinfo()[1] + "\n\t\t\t</p>\n\t\t\t<p>\n\t\t\t\tLocation: " + person.myinfo()[2] + "\n\t\t\t</p>\n\t\t\t<p>\n\t\t\t\tHobbies: " + person.myinfo()[3] + "\n\t\t\t</p>\n\t\t\t<p>\n\t\t\t\tFavourite Music: " + person.myinfo()[4] + "\n\t\t\t</p>\n \t\t</div>\n \t</div>";
 }
--- Catalin/myscript.ts
+++ Catalin/myscript.ts
@@ -18,18 +18,68 @@ class Person{
 	}
 
 	myinfo(){
-		return [this.name, this.age, this.location, this.hobbies, this.music, this.img];
+		return [this.name, this.age, this.location, this.hobbies, this.music];
 
 	}
 
 	myquote(){
-		return [this.qoute, this.img];
+		return this.qoute;
+	}
+
+	myimage(){
+		return this.img
 	}
 
 }
  var Profile = JSON.parse(json);
 
- var person1= new Person(Profile[0].name,Profile[0].age, Profile[0].location, Profile[0].hobbies, Profile[0].music, Profile[0].qoute, Profile[0].img);
+ //var person1= new Person(Profile[0].name,Profile[0].age, Profile[0].location, Profile[0].hobbies, Profile[0].music, Profile[0].qoute, Profile[0].img);
 
- console.log(person1.myinfo());
- console.log(person1.myquote());
+ //console.log(person1.myinfo());
+ //console.log(person1.myquote());
+
+
+//${person.myimage()}>
+ for (let index in Profile){
+
+ 	let person = new Person(Profile[index].name,Profile[index].age, Profile[index].location, Profile[index].hobbies, Profile[index].music, Profile[index].qoute, Profile[index].img)
+ 	
+
+ 	document.getElementById("wrapper1").innerHTML +=
+ 	`<div id = "card_${index}" class = "cardSmall"> 
+ 		<div id = "imageBox_${index}" class = "imageBoxBig">
+ 			<img id = "image_${index}" src = "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+ 		</div>
+ 		<div id = "quoteBox_${index}">
+			<h3> 
+				${person.myquote()}
+			</h3>
+ 		</div>
+ 	</div>`
+
+ 	document.getElementById("wrapper2").innerHTML +=
+ 	`<div id = "cardFav_${index} class = "cardBig"> 
+ 		<div id = "imageBoxFav_${index}" class = "imageBoxBig">
+ 			<img id = "imageFav_${index}" src = "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500">
+ 		</div>
+ 		<div id = "infoBox_${index}">
+			<h4> 
+				Name: ${person.myinfo()[0]}
+			</h4>
+			<p>
+				Age: ${person.myinfo()[1]}
+			</p>
+			<p>
+				Location: ${person.myinfo()[2]}
+			</p>
+			<p>
+				Hobbies: ${person.myinfo()[3]}
+			</p>
+			<p>
+				Favourite Music: ${person.myinfo()[4]}
+			</p>
+ 		</div>
+ 	</div>`
+
+
+ }
